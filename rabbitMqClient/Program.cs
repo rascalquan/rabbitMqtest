f674c6cec742e9c910ba32e1d2735950ad554205
YangXiaoQuan <yangxiaoquan4402@dingtalk.com>
2020-08-27 08:59:09 +0800

    init

---
+++ rabbitMqClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace rabbitMqClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exchangeName = "rabbittopictest";
+            string queueName = "sayhello";
+            string routeKey = "sayhello.2";
+
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "iccode.top"
+            };
+            //创建连接
+            var connection = factory.CreateConnection("rabbitmqclient");
+            //创建管道
+            var channel = connection.CreateModel();
+
+            //声明队列
+            channel.QueueDeclare(queueName, false, false, false, null);
+
+            channel.QueueBind(queueName, exchangeName, routeKey, null);
+
+            //channel.ConfirmSelect();//回执确认，手动确认
+
+            //事件基本消费者
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine($"收到消息:{message}");
+                //确认消息
+                channel.BasicAck(e.DeliveryTag, false);
+                //Console.WriteLine("已发送回执");
+            };
+
+            //启动消费者
+            channel.BasicConsume(queueName, false, consumer);
+            Console.WriteLine("消费者已启动");
+            Console.ReadKey();
+            channel.Dispose();
+            connection.Close();
+        }
+    }
+}
---
+++ rabbitMqServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace rabbitMqServer
+{
+    class Program
+    {
+        //服务端/生产者只需要关心交换机，无需关心queue，不用声明队列，
+        //消费者也只需关心queue，无需关心交换机
+        static void Main(string[] args)
+        {
+            string exchangeName = "rabbittopictest";
+
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "iccode.top"
+            };
+
+            //创建连接
+            var connection = factory.CreateConnection("rabbitmqserver");
+            //创建通道
+            var channel = connection.CreateModel();
+            //定义交换机
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, false, false, null);
+            ////声明队列
+            //channel.QueueDeclare(queueName, false, false, false, null);
+
+            //channel.QueueBind(queueName, exchangeName, routeKey, null);
+
+
+            Console.WriteLine("\nRabbitMQ连接成功，请输入消息，输入exit退出！");
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+
+                var sendByte = Encoding.UTF8.GetBytes(input);
+                
+                //发布消息
+                var topicName = "sayhello." + new Random().Next(1, 5);
+                Console.WriteLine("Topic:" + topicName);
+                channel.BasicPublish(exchangeName, topicName, null, sendByte);
+
+            } while (input.Trim().ToLower() != "exit");
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
